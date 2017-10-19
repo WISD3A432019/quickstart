7cf34799e5ae92e28d031cfc399a107fc0c2fd56
jkluio132465 <jkluio132465@gmail.com>
2017-10-19 18:33:41 +0800

    顯示目前任務

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -32,5 +32,26 @@
         </form>
     </div>
 
-    <!-- 代辦：目前任務 -->
+       <!-- 目前任務 -->
+    @if (count($tasks) > 0)
+    <tbody>
+        @foreach ($tasks as $task)
+            <tr>
+                <!-- 任務名稱 -->
+                <td class="table-text">
+                    <div>
+                        {{ $task->name }}
+                    </div>
+                </td>
+                
+                <td>
+                 <!-- 代辦：刪除按鈕 -->
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+    @endif
+
+  
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,12 @@ use Illuminate\Http\Request;
 
 //顯示我們所有任務的清單
 Route::get('/', function () {
-        return view('tasks');
+    $tasks = App\Task::orderBy('created_at', 'asc')->get();
+             //利用model Task由DB的tasks資料表取出資料
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);       //將tasks資料表取出的資料傳遞給tasks視圖
+
 });
 
 // 增加新的任務
@@ -37,5 +42,8 @@ Route::post('/task', function (Request $request) {
 
 // 刪除任務
 Route::delete('/task/{task}', function (Task $task) {
-        //
+    
+    //$task->delete();
+    //return redirect('/');
+
 });
